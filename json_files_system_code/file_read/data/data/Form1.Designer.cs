5cd548d3dea23877a44192b2f935b072476ed96b
Sefa Sayracı <73780930+Sefasayraci@users.noreply.github.com>
2023-07-09 13:49:16 +0300

    Add files via upload

---
+++ json_files_system_code/file_read/data/data/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace data
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.grdListe = new System.Windows.Forms.DataGridView();
+            this.tmrGuncelle = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.grdListe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "File Selected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(37, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(207, 281);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(280, 45);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(207, 281);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(528, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(207, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "JSON to XML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(815, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 77);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "JSON Saved";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(815, 336);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 77);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(93, 10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(815, 239);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(175, 77);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "XML Saved";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(528, 44);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(207, 282);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = "";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(996, 239);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(207, 50);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "JSON List";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(815, 142);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(175, 76);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "JSON List Saved";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // grdListe
+            // 
+            this.grdListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdListe.Location = new System.Drawing.Point(996, 44);
+            this.grdListe.Name = "grdListe";
+            this.grdListe.RowHeadersWidth = 51;
+            this.grdListe.RowTemplate.Height = 29;
+            this.grdListe.Size = new System.Drawing.Size(300, 188);
+            this.grdListe.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1295, 521);
+            this.Controls.Add(this.grdListe);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdListe)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private OpenFileDialog openFileDialog1;
+        private Button button5;
+        private Button button6;
+        private RichTextBox richTextBox3;
+        private Button button7;
+        private Button button8;
+        private DataGridView grdListe;
+        private System.Windows.Forms.Timer tmrGuncelle;
+    }
+}
---
+++ json_files_system_code/file_read/data/data/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System.Drawing.Imaging;
+using System.IO;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+//using JsonConveter;
+//using Json;
+using System.Windows;
+using System.Windows.Forms;
+using System.IO.Pipes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Seçmeden okuma üst code, alt code seçerek okumadır.
+        {
+            /*
+            try
+            {
+              //  StreamReader oku = new StreamReader(@"C:\Users\sefas\Desktop\mmu.json");
+                StreamReader oku = new StreamReader("readfile");
+                richTextBox1.Text = oku.ReadToEnd();
+                oku.Close();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("dosya okunmadı");
+
+            }
+            */
+            try
+            {
+                openFileDialog1.ShowDialog();
+                string filename = openFileDialog1.FileName;
+                string readfile = File.ReadAllText(filename);
+                richTextBox1.Text = readfile;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("dosya okunmadı");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // JSON to XML Gelecek_Convert_Data
+        {/*
+            try
+            {
+                using (Converter converter = new Converter(@"C:\Users\sefas\Desktop\mmu.json"))
+                {
+                    DataConvertOptions options = new DataConvertOptions
+                    {
+                        Format = DataFileType.Xml
+                    };
+                    converter.Convert(@"path/jsonToXML.xml", options);
+                }
+            }
+            catch (Exception) 
+            {
+                MessageBox.Show("dosya cevrilmedi");         
+            }
+           // XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(json);*/
+        }
+
+        private void button3_Click(object sender, EventArgs e) // JSON Saved Select
+        {
+            string metin = richTextBox1.Text;
+            FileStream fileStream = new FileStream("mmujson.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fileStream);
+            sw.WriteLine(metin);
+            sw.Close();
+            MessageBox.Show("Kayıt Başarılı", "Dosya Kayıt İşlemi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            /*
+            string metin2 = richTextBox2.Text;
+            FileStream fileStream2 = new FileStream("sefa2.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw2 = new StreamWriter(fileStream);
+            sw.WriteLine(metin);
+            sw.Close();
+            MessageBox.Show("Kayıt Başarılı", "Dosya Kayıt İşlemi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            */
+        }
+
+        private void button4_Click(object sender, EventArgs e) // Clear in textbox.rich
+        {
+            string metin = richTextBox1.Text;
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Okuma Kısmı Üst Kısma Geçirildi. Ondan Okunmuyor. Gereksiz
+        {/*
+            string[] secilendosyayolu;
+            openFileDialog1.ShowDialog();
+            secilendosyayolu = openFileDialog1.FileNames;
+
+            for (int i = 0; i < secilendosyayolu.Length; i++)
+            {
+ 
+                OpenFileDialog file = new OpenFileDialog();
+                file.FilterIndex = 2;
+                file.RestoreDirectory = true;
+                file.CheckFileExists = false;
+                file.Title = "Json File Dosyası Açınız";
+                file.Multiselect = true;
+
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    string DosyaYolu = file.FileName;
+                    string DosyaAdi = file.SafeFileName;
+                }
+            }
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            string readfile = File.ReadAllText(filename);
+            richTextBox2.Text = readfile;
+          */
+        }
+
+        private void button7_Click(object sender, EventArgs e) // JSON List Selected
+        {
+           // JsonListe();
+        }
+
+        private void button6_Click(object sender, EventArgs e)  // XML Saved Select
+        {
+            string metin = richTextBox3.Text;
+            FileStream fileStream = new FileStream("mmuxml.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fileStream);
+            sw.WriteLine(metin);
+            sw.Close();
+            MessageBox.Show("Kayıt Başarılı", "Dosya Kayıt İşlemi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button8_Click(object sender, EventArgs e)  // JSON List Saved
+        {
+            string metin = richTextBox2.Text;
+            FileStream fileStream = new FileStream("mmujsonlist.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fileStream);
+            sw.WriteLine(metin);
+            sw.Close();
+            MessageBox.Show("Kayıt Başarılı", "Dosya Kayıt İşlemi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        /*public List<JsonModel> JsonListe()
+        {
+            List<JsonModel> LsonModelList = null;
+            try
+            {
+                StreamReader oku = new StreamReader(@"C:\Users\sefas\Desktop\mmu.json"); // deneme için okunmuş dosya çekilmeli
+                //richTextBox1.Text = oku.ReadToEnd();
+                
+                JsonModelList = Newtonsoft.Json.JsonConvert.SerializeObject<List<JsonModel>>(JsonText);
+                //oku.Close();
+            }
+            catch (Exception)
+            {
+                JsonModelList = null;
+
+            }
+            return JsonModelList;
+        }
+        public class RootoObject // Başlangıç
+        {
+
+            public class Rootobject
+            {
+                public int IcdParameterId { get; set; }
+                public string IrsParameterName { get; set; }
+                public string DataType { get; set; }
+                public float Default { get; set; }
+                public bool DTSUpdate { get; set; }
+                public bool GlobalReset { get; set; }
+                public bool ElectronicsReset { get; set; }
+                public bool Nvm { get; set; }
+                public bool Monitor { get; set; }
+                public bool IsConstantParameter { get; set; }
+                public int[] RelatedSources { get; set; }
+                public int IrsParameterId { get; set; }
+                public string IcdParameterName { get; set; }
+                public Parameterrange ParameterRange { get; set; }
+                public string Direction { get; set; }
+                public string Unit { get; set; }
+            }
+
+            public class Parameterrange
+            {
+                public string RangeType { get; set; }
+                public float MinValue { get; set; }
+                public float MaxValue { get; set; }
+            }
+            
+        }//bitiş*/
+    }
+}
+
+
+// son not bu önemli şimdi bir listeleme seçeneği kaldı bir de json to xml sadece bunlar kaldı bir en son json to xml clear gelir
+// Temiz kod için buton isimleri app kullanışına göre çevir
+// Button 7 ve List Saved kısmında işlem yaptım.
